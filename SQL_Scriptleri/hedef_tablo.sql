4a0123ca25546b01b184837f4a30bba34e305131
sametyildirim314 <sametyldrmckrxwz@gmail.com>
2026-06-06 13:47:28 +0300

    2. ADIM: HEDEF TABLO OLUŞTURMA (LOAD İÇİN HAZIRLIK)

---
+++ SQL_Scriptleri/hedef_tablo.sql
@@ -0,0 +1,11 @@
+
+
+
+--2. ADIM: HEDEF TABLO OLUŞTURMA (LOAD İÇİN HAZIRLIK)
+CREATE TABLE dbo.Hedef_TemizMusteri (
+    ID INT,
+    AdSoyad_Temiz NVARCHAR(100),
+    Telefon_Temiz NVARCHAR(50),
+    Eposta_Durumu NVARCHAR(100)
+);
+GO
